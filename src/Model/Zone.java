b0984637fd2cb8c2c13e7d7c6a2f6746d15b7e69
Administrator <Administrator@PC7>
2015-10-25 14:56:54 +0400

    New Model

---
+++ src/Model/Zone.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Zone {
+	
+	private int id_zone;
+	private String zone;
+	
+	public int getId_zone() {
+		return id_zone;
+	}
+	
+	public void setId_zone(int id_zone) {
+		this.id_zone = id_zone;
+	}
+	
+	public String getZone() {
+		return zone;
+	}
+	
+	public void setZone(String zone) {
+		this.zone = zone;
+	}
+
+}
