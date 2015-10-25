3f168e8cf545a20125b96a308e8e573515fab9ee
darking <darkling@hotmail.com>
2015-10-25 14:11:28 +0300

    wave classes

---
+++ src/Model/Wave_Date.java
@@ -0,0 +1,53 @@
+package Model;
+
+import java.util.Date;
+
+public class Wave_Date {
+
+	int id_wave_date;
+	String wave;
+	Date date;
+	
+	
+	public Wave_Date(int id_wave_date, String wave, Date date) {
+		super();
+		this.id_wave_date = id_wave_date;
+		this.wave = wave;
+		this.date = date;
+	}
+
+
+	public int getId_wave_date() {
+		return id_wave_date;
+	}
+
+
+	public void setId_wave_date(int id_wave_date) {
+		this.id_wave_date = id_wave_date;
+	}
+
+
+	public String getWave() {
+		return wave;
+	}
+
+
+	public void setWave(String wave) {
+		this.wave = wave;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+}
