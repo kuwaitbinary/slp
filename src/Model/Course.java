01bc91401c7e7ee50df2eca8b054bc8b7b626005
aisha-al-ansari@hotmail.com <aisha-al-ansari@hotmail.com>
2015-10-25 15:22:40 +0400

    added classes

---
+++ src/Model/Course.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class Course {
+private int id_course ;
+private String name ;
+private String syllabus ;
+private int duration ;
+
+
+
+public int getDuration() {
+	return duration;
+}
+
+public void setDuration(int duration) {
+	this.duration = duration;
+}
+
+public String getSyllabus() {
+	return syllabus;
+}
+
+public void setSyllabus(String syllabus) {
+	this.syllabus = syllabus;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public int getI() {
+	return id_course;
+}
+
+public void setI(int i) {
+	this.id_course = i;
+}
+
+}
---
+++ src/Model/Reg_Session.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class Reg_Session {
+
+public int id_reg_session;
+public String id_class;
+public String id_trainee;
+
+
+
+public int getId_reg_session() {
+	return id_reg_session;
+}
+public void setId_reg_session(int id_reg_session) {
+	this.id_reg_session = id_reg_session;
+}
+
+
+public String getId_class() {
+	return id_class;
+}
+public void setId_class(String id_class) {
+	this.id_class = id_class;
+}
+
+
+public String getId_trainee() {
+	return id_trainee;
+}
+public void setId_trainee(String id_trainee) {
+	this.id_trainee = id_trainee;
+}
+	
+	
+	
+	
+}
