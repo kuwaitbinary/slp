13eb9be3e3ad316ccad1dfdf742b1685a41992b0
Administrator <Administrator@PC7>
2015-10-25 14:34:54 +0400

    Add servlet classes

--- src/WebServices/LoginServlet.java
+++ src/WebServices/LoginServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Login
  */
 @WebServlet("/Login")
-public class Login extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public Login() {
+    public LoginServlet() {
         // TODO Auto-generated constructor stub
     }
 
