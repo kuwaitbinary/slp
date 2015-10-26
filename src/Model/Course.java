4c3f06d2c1069b8b1b6514032435638296c9eac1
CSOMAR <omar@alobaid.co>
2015-10-26 15:23:28 +0400

    Update Model

--- src/Model/Course.java
+++ src/Model/Course.java
@@ -1,43 +1,68 @@
 package Model;
 
+import java.util.ArrayList;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Course {
-private int id_course ;
-private String name ;
-private String syllabus ;
-private int duration ;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_course;
+	
+	private String name;
+	
+	private String syllabus;
+	
+	private int duration;
+	
+	@OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+	private ArrayList<Wave> waves;
+	
+	public int getDuration() {
+		return duration;
+	}
+	
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	public String getSyllabus() {
+		return syllabus;
+	}
+	
+	public void setSyllabus(String syllabus) {
+		this.syllabus = syllabus;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getI() {
+		return id_course;
+	}
+	
+	public void setI(int i) {
+		this.id_course = i;
+	}
 
+	public ArrayList<Wave> getWaves() {
+		return waves;
+	}
 
-
-public int getDuration() {
-	return duration;
-}
-
-public void setDuration(int duration) {
-	this.duration = duration;
-}
-
-public String getSyllabus() {
-	return syllabus;
-}
-
-public void setSyllabus(String syllabus) {
-	this.syllabus = syllabus;
-}
-
-public String getName() {
-	return name;
-}
-
-public void setName(String name) {
-	this.name = name;
-}
-
-public int getI() {
-	return id_course;
-}
-
-public void setI(int i) {
-	this.id_course = i;
-}
+	public void setWaves(ArrayList<Wave> waves) {
+		this.waves = waves;
+	}
 
 }
