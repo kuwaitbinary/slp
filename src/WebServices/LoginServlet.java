b92f0ecfd28272764723aff7912f482ccac85ebe
darking <darkling@hotmail.com>
2015-10-26 11:36:37 +0300

    wave Edited

--- src/WebServices/LoginServlet.java
+++ src/WebServices/LoginServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DaoImplementation.UserProfileDoaImpl;
+
 /**
  * Servlet implementation class Login
  */
@@ -26,6 +28,9 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		UserProfileDoaImpl us = new UserProfileDoaImpl();
+		us.getMyWayEntityManager();
 	}
 
 	/**
