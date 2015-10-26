d74ddc01128b4c01b936a1350d93d7fbbda18192
CSOMAR <omar@alobaid.co>
2015-10-26 14:57:13 +0400

    Update Model

--- src/Model/Location.java
+++ src/Model/Location.java
@@ -1,36 +1,69 @@
 package Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Location {
-private int id_location;
-private String location;
-private String gps;
-private int id_zone;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_location;
+	
+	private String location;
+	
+	private String gps;
+	
+	@Column(columnDefinition = "id_zone")
+	@ManyToOne
+	private Zone zone;
+	
+	@OneToOne(mappedBy = "location")
+	private Active_Session activeSession;
+	
+	public int getId_location() {
+		return id_location;
+	}
+	
+	public void setId_location(int id_location) {
+		this.id_location = id_location;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public String getGps() {
+		return gps;
+	}
+	
+	public void setGps(String gps) {
+		this.gps = gps;
+	}
 
+	public Zone getZone() {
+		return zone;
+	}
 
+	public void setZone(Zone zone) {
+		this.zone = zone;
+	}
 
-public int getId_location() {
-	return id_location;
-}
-public void setId_location(int id_location) {
-	this.id_location = id_location;
-}
-public String getLocation() {
-	return location;
-}
-public void setLocation(String location) {
-	this.location = location;
-}
-public String getGps() {
-	return gps;
-}
-public void setGps(String gps) {
-	this.gps = gps;
-}
-public int getId_zone() {
-	return id_zone;
-}
-public void setId_zone(int id_zone) {
-	this.id_zone = id_zone;
-}
+	public Active_Session getActiveSession() {
+		return activeSession;
+	}
+
+	public void setActiveSession(Active_Session activeSession) {
+		this.activeSession = activeSession;
+	}
 
 }
--- src/Model/Zone.java
+++ src/Model/Zone.java
@@ -1,9 +1,25 @@
 package Model;
 
+import java.util.ArrayList;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Zone {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id_zone;
+	
 	private String zone;
+	
+	@OneToMany(mappedBy = "zone", cascade = CascadeType.ALL)
+	private ArrayList<Location> locations;
 	
 	public int getId_zone() {
 		return id_zone;
@@ -19,6 +35,14 @@ public class Zone {
 	
 	public void setZone(String zone) {
 		this.zone = zone;
+	}
+
+	public ArrayList<Location> getLocations() {
+		return locations;
+	}
+
+	public void setLocations(ArrayList<Location> locations) {
+		this.locations = locations;
 	}
 
 }
