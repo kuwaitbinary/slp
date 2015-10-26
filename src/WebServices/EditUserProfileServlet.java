e06946202516977b534b441e4a2311f60e17dd52
ShBouyabes <Sh.Bouyabes@gmail.com>
2015-10-26 09:08:11 +0300

    added servlets

--- src/WebServices/EditUserProfileServlet.java
+++ src/WebServices/EditUserProfileServlet.java
@@ -1,16 +1,23 @@
 package WebServices;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DaoImplementation.EditProfileDaoImpl;
+import Model.Trainee;
+
+import com.ibm.json.java.JSONArray;
+import com.ibm.json.java.JSONObject;
+
 /**
  * Servlet implementation class EditUserProfileServlet
  */
-@WebServlet("/EditUserProfileServlet")
+@WebServlet("/EditUserProfile")
 public class EditUserProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,6 +34,7 @@ public class EditUserProfileServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		editProfile(request, response);
 	}
 
 	/**
@@ -34,6 +42,57 @@ public class EditUserProfileServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		editProfile(request, response);
+	}
+	
+	void editProfile(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		
+		int id = Integer.parseInt((String)request.getParameter("idTrainee"));
+		int newId = Integer.parseInt((String)request.getParameter("newIdTrainee"));
+		String name = (String)request.getParameter("name");
+		String mobile = (String)request.getParameter("mobile");
+		String email = (String)request.getParameter("email");
+		String subject = (String)request.getParameter("subject");
+		
+		EditProfileDaoImpl ep = new EditProfileDaoImpl();
+		
+		Trainee t = new Trainee();
+		
+		t.setId_Trainee(newId);
+		t.setFirstname(name);
+		t.setMobile(mobile);
+		t.setEmail(email);
+		t.setSubject(subject);
+		
+		ep.editUserProfile(id, t);
+		
+		JSONObject json = new JSONObject();
+		json.put("message", "success");
+		json.put("result_code", 0);
+		
+		
+		JSONArray result_data = new JSONArray();
+
+		//for(int i = 0;i<favorites.size();i++) {
+			
+			JSONObject jsonReport = new JSONObject();
+			
+			
+			//jsonReport.put("name", f.getName());
+			//jsonReport.put("latitude", f.getLatitude());
+			//jsonReport.put("longitude", f.getLongitude());
+			
+			result_data.add(jsonReport);
+		//}
+		
+		json.put("result_data", result_data);
+		
+		
+		response.getWriter().print(json);
+
+		
+		
+		
 	}
 
 }
